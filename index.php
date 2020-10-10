e1876a823101ffce7d9c58b75d11e0c31e85e39f
juangovi <65275993+juangovi@users.noreply.github.com>
2020-10-10 21:34:11 +0200

    registro
    nueva pagina de registro

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'><link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<div class="container h-100vh">
+	<div class="row row h-100 align-items-center justify-content-centerr">
+		<div class="col align-self-cente ">
+			<div class="card">
+				<div class="card-header text-center display-4">
+					registro
+				</div>
+				<div class="card-body">
+					<form id="xd" method="post" action="">
+                    <div class="form-group">
+							<label for="email">nick</label>
+							<input type="text" class="form-control" id="email" required placeholder="Enter email">
+						</div>
+
+						<div class="form-group">
+							<label for="email">Email address</label>
+							<input type="email" class="form-control" id="email" required placeholder="Enter email">
+                        </div>
+
+                        <div class="form-group">
+							<label for="password">Password</label>
+							<input type="password" class="form-control" id="password" required placeholder="Password">
+							
+                        </div>
+                        
+						<div class="form-group>">
+							<label for="password" id="ojo">confir Password</label>
+							<input type="password" class="form-control" id="conpassword" required placeholder="Password">
+							
+                        </div>
+                        <p id="mensaje" style="color: #fa0505">
+                           
+                        </p>
+                        <button type="button" id="boton" class="btn btn-primary btn-lg btn-block" onclick="comprobar()">registrar</button>
+                        <script>
+                            function comprobar(){
+                            var con1=document.getElementById("password").value;
+                            var con2=document.getElementById("conpassword").value;
+                            if (con1==con2) {
+                                document.getElementById("boton").type="submit";
+                                document.getElementById("mensaje").innerHTML="";
+                            }else{
+                            document.getElementById("mensaje").innerHTML="contraseña no coinciden";
+                            document.getElementById("boton").type="button";
+                            }
+                            }
+                        </script>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
---
+++ plantilla.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<script language="javascript">
+</script>
+</head>
+</html>
+document.write("");
+var numero=prompt()
+.meses{
+			width: 7.3%; 
+			float: left;
+			padding-left: 1%;
+
+		}
+		.pri{
+			width: 7.3%; 
+			float: left;
+			background-color:blue;
+			padding-left: 1%;
+		}
+		.cumple{
+			background-color: red;
+			width: 7.3%;
+			float: left;
+			padding-left: 1%;
+		}
+		}
+<form action="multiplicar.php" method="get">
+escribe un numerico xd<input type="text" name="num" placeholder="12"><br>
+escribe otro numerico<input type="text" name="veces" placeholder="123"><br>
+<input type="submit">
+
+</form>
+$frase=$_GET["num"];
+
+
+if(isset($_POST["frase"])){
+$frase=$_GET["frase"];
+}
+else{
+	$frase="";
+}
+echo "<tr>";
+        echo "<td>".$row['id_usuario']."</td><td>".$row['nombre']."</td><td>".$row['apellido1']."</td<td>".$row['apellido2']."</td><td>".$row['direcion']."</td><td>".$row['correo_electronico']."</td><td>".$row['telefono']."</td> <td>".$row['Fecha_de_nacimiento']."</td><td>".$row['NIF']."</td><td>".$row['poblacion']."</td><td>".$row['provincia']."</td><td>".$row['password']."</td></tr>" ;
+}
+
+
+
+ echo "<table border='1'>";
+   echo "<tr>";
+        echo "<td>id_usuario</td><td>nombre</td><td>primer apellido</td><td>segindo apellido</td><td>direccion</td><td>correo electronico</td><td>telefono</td><td>fecha de nacimiento</td><td>NIF</td><td>poblacion</td><td>provincia</td><td>password</td>";
+    echo "</tr>";
+    <select name="grado">
+    	<option value="xd"></option>
+    	<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Radio Buttons</h2>
+
+<form>
+  <input type="radio" id="male" name="gender" value="male">hola <br>
+ 
+  <input type="radio" id="female" name="gender" value="female"> paco<br>
+ 
+  <input type="radio" id="other" name="gender" value="other">jos<br>
+  
+  <input type="radio" id="other" name="hose" value="other">jos<br>
+  
+</form> 
+<img src="" width="">
+</body>
+</html>
+<form action="consula.php"method="post">
+	<input type="submit" value="consulta"><br>
+	dni<input type="text" name="nombre"><br>
+</form>
