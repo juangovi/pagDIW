995dee732d728a2de4d73f2039bc27fb991e323f
juangovi <65275993+juangovi@users.noreply.github.com>
2020-11-06 10:04:01 +0100

    imagen
    subir y mostrar imagen

--- pag.php
+++ pag.php
@@ -32,6 +32,24 @@ session_start();
         <input type="hidden" name="salir" value="1">
         <input type="submit" value="cerrar sesion">
     </form>
+    <form action="pag.php" method="post" enctype="multipart/form-data">
+     cambiar imagen:
+    <input type="file" name="fileToUpload" id="fileToUpload"><br>
+    <input type="submit" value="subir" name="aceptar">
+    </form>
+    <?php
+        include("subirfoto.php");
+        $img=obtenerimg($datos["Usuario_nick"]);
+        if($img==null||$img==""){
+            $img="default.jpg";
+        }
+        echo "<img src='fotosperfil/".$img."' alt='f'>";
+        if(isset($_POST["aceptar"])){
+        
+        crearimg($datos["Usuario_nick"]);
+        
+        }
+    ?>
 </body>
 
 </html>
