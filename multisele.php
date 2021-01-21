c46f3abb8c3178e6a2c0d389e7566fe830f34e52
juangovi <65275993+juangovi@users.noreply.github.com>
2021-01-21 13:02:30 +0100

    delete multiple

--- multisele.php
+++ multisele.php
@@ -1,8 +1,12 @@
 <?php
 // Start the session
-
+include("coneccion.php");
+$conn=conectar();
 if(isset($_POST["multiselect"])){
     $valores=$_POST["multiselect"];
-    echo "se selecionaron ".count($valores)." proeductos";
-
+    echo "se eliminaron ".count($valores)." proeductos";
+    for ($i=0; $i < count($valores); $i++) { 
+        $sql = "DELETE FROM productos WHERE id =".$valores[$i];
+        $result = $conn->query($sql);
+    }
 }
